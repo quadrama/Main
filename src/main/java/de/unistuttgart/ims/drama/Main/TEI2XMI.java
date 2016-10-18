a93772e224509fa2e93918254ae0fca714496641
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-10-18 15:59:44 +0200

    main class to process tei to create xmi

---
+++ src/main/java/de/unistuttgart/ims/drama/Main/TEI2XMI.java
@@ -0,0 +1,57 @@
+package de.unistuttgart.ims.drama.Main;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.io.File;
+
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import com.lexicalscope.jewel.cli.CliFactory;
+import com.lexicalscope.jewel.cli.Option;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.tudarmstadt.ukp.dkpro.core.languagetool.LanguageToolSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
+import de.unistuttgart.quadrama.core.DramaSpeechSegmenter;
+import de.unistuttgart.quadrama.core.FigureMentionDetection;
+import de.unistuttgart.quadrama.core.FigureReferenceAnnotator;
+import de.unistuttgart.quadrama.core.SpeakerIdentifier;
+import de.unistuttgart.quadrama.io.tei.textgrid.TextgridTEIUrlReader;
+
+public class TEI2XMI {
+
+	public static void main(String[] args) throws Exception {
+		Options options = CliFactory.parseArguments(Options.class, args);
+
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
+				TextgridTEIUrlReader.PARAM_INPUT_DIRECTORY, options.getInputDirectory());
+
+		AggregateBuilder builder = new AggregateBuilder();
+
+		builder.add(DramaSpeechSegmenter.getWrappedSegmenterDescription(LanguageToolSegmenter.class));
+		builder.add(createEngineDescription(FigureReferenceAnnotator.class));
+		builder.add(
+				createEngineDescription(SpeakerIdentifier.class, SpeakerIdentifier.PARAM_CREATE_SPEAKER_FIGURE, true));
+		builder.add(createEngineDescription(StanfordPosTagger.class));
+		builder.add(createEngineDescription(StanfordNamedEntityRecognizer.class));
+		builder.add(createEngineDescription(FigureMentionDetection.class));
+
+		builder.add(createEngineDescription(XmiWriter.class, XmiWriter.PARAM_TARGET_LOCATION,
+				options.getOutputDirectory()));
+
+		SimplePipeline.runPipeline(reader, builder.createAggregateDescription());
+	}
+
+	public interface Options {
+		@Option
+		File getInputDirectory();
+
+		@Option
+		File getOutputDirectory();
+	}
+
+}
