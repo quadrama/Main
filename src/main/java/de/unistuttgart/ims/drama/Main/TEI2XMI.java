5009a27dfcc0f4df514e3821c19f24fd6c859123
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-10-31 20:40:56 +0100

    updating DramaNLP

--- src/main/java/de/unistuttgart/ims/drama/Main/TEI2XMI.java
+++ src/main/java/de/unistuttgart/ims/drama/Main/TEI2XMI.java
@@ -2,12 +2,15 @@ package de.unistuttgart.ims.drama.Main;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 
+import java.io.File;
+
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AggregateBuilder;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 
 import com.lexicalscope.jewel.cli.CliFactory;
+import com.lexicalscope.jewel.cli.Option;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.tudarmstadt.ukp.dkpro.core.matetools.MateLemmatizer;
@@ -17,16 +20,18 @@ import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.unistuttgart.quadrama.core.DramaSpeechSegmenter;
 import de.unistuttgart.quadrama.core.FigureMentionDetection;
 import de.unistuttgart.quadrama.core.FigureReferenceAnnotator;
+import de.unistuttgart.quadrama.core.ReadDlinaMetadata;
+import de.unistuttgart.quadrama.core.SetReferenceDate;
 import de.unistuttgart.quadrama.core.SpeakerIdentifier;
 import de.unistuttgart.quadrama.io.tei.textgrid.TextgridTEIUrlReader;
 
 public class TEI2XMI {
 
 	public static void main(String[] args) throws Exception {
-		Options options = CliFactory.parseArguments(Options.class, args);
+		MyOptions options = CliFactory.parseArguments(MyOptions.class, args);
 
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
-				TextgridTEIUrlReader.PARAM_INPUT_DIRECTORY, options.getInput());
+				TextgridTEIUrlReader.PARAM_INPUT, options.getInput(), TextgridTEIUrlReader.PARAM_CLEANUP, true);
 
 		AggregateBuilder builder = new AggregateBuilder();
 
@@ -34,6 +39,11 @@ public class TEI2XMI {
 		builder.add(createEngineDescription(FigureReferenceAnnotator.class));
 		builder.add(
 				createEngineDescription(SpeakerIdentifier.class, SpeakerIdentifier.PARAM_CREATE_SPEAKER_FIGURE, true));
+		if (options.getDlinaDirectory() != null) {
+			builder.add(createEngineDescription(ReadDlinaMetadata.class, ReadDlinaMetadata.PARAM_DLINA_DIRECTORY,
+					options.getDlinaDirectory()));
+			builder.add(createEngineDescription(SetReferenceDate.class));
+		}
 		builder.add(createEngineDescription(StanfordPosTagger.class));
 		builder.add(createEngineDescription(MateLemmatizer.class));
 		builder.add(createEngineDescription(StanfordNamedEntityRecognizer.class));
@@ -44,4 +54,8 @@ public class TEI2XMI {
 		SimplePipeline.runPipeline(reader, builder.createAggregateDescription());
 	}
 
+	interface MyOptions extends Options {
+		@Option(defaultToNull = true)
+		File getDlinaDirectory();
+	}
 }
